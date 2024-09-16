9698aae6b3eca7cdbab647ff235a241b05384562
Soren Geiger <soerengeiger@gmx.de>
2024-09-16 18:13:33 +0200

    added parsing for textures

--- parsing/parsing_texture.c
+++ parsing/parsing_texture.c
@@ -6,7 +6,7 @@
 /*   By: sgeiger <sgeiger@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 16:28:13 by sgeiger           #+#    #+#             */
-/*   Updated: 2024/09/09 18:43:14 by sgeiger          ###   ########.fr       */
+/*   Updated: 2024/09/14 19:09:48 by sgeiger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,23 @@ int	count_space(char *line)
 	return (i);
 }
 
-bool	is_texture(char *line)
+bool	is_texture(t_game *game, char *line)
 {
-	if (!ft_strncmp(line, "NO ", 3) || !ft_strncmp(line, "SO ", 3)
-		|| !ft_strncmp(line, "WE ", 3) || !ft_strncmp(line, "EA ", 3))
-	{
+	int	i;
+
+	i = 0;
+	while (ft_isspace(line[i]))
+		i++;
+	if (ft_strlen(line + i) < 4)
+		return (false);
+	if (!ft_strncmp(line + i, "NO ", 3) && !game->textures->no_texture)
 		return (true);
-	}
+	else if (!ft_strncmp(line + i, "SO ", 3) && !game->textures->so_texture)
+		return (true);
+	else if (!ft_strncmp(line + i, "WE ", 3) && !game->textures->we_texture)
+		return (true);
+	else if (!ft_strncmp(line + i, "EA ", 3) && !game->textures->ea_texture)
+		return (true);
 	else
 		return (false);
 }
@@ -37,6 +47,7 @@ void	create_texture(char *path, mlx_texture_t **texture)
 {
 	if (!*texture)
 		*texture = mlx_load_png(path);
+	// printf("-%s\n", path);
 	free(path);
 	return ;
 }
@@ -45,18 +56,13 @@ void	check_textures(t_game *game, char *line)
 {
 	int		fd;
 	int		space;
-	size_t	len;
 	char	*path;
 
-	len = ft_strlen(line);
-	if (len < 4)
-		return ;
 	space = count_space(line + 2);
-	path = ft_substr(line, space + 2, len - space - 2);
-	printf("-%s-", path);
+	path = ft_substr(line, space + 2, ft_strlen(line) - space - 2);
 	fd = open(path, O_RDONLY);
 	if (fd < 0)
-		return ;
+		terminate(game);
 	if (line[0] == 'N')
 		create_texture(path, &game->textures->no_texture);
 	else if (line[0] == 'S')
