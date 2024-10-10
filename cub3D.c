95824c67d957fb0f9ea8836bfde5035d9830eda9
Xyloo <adprzyby@student.42heilbronn.de>
2024-10-10 13:54:55 +0200

    added commented function for checking leaks

--- cub3D.c
+++ cub3D.c
@@ -6,11 +6,19 @@
 /*   By: adprzyby <adprzyby@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/21 12:53:39 by adprzyby          #+#    #+#             */
-/*   Updated: 2024/09/28 18:59:27 by adprzyby         ###   ########.fr       */
+/*   Updated: 2024/10/10 13:53:52 by adprzyby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
+
+// void check_for_leaks(void)
+// {
+//     char command[256];
+//     pid_t pid = getpid();
+//     sprintf(command, "leaks %d", pid);
+//     system(command);
+// }
 
 int	main(int argc, char *argv[])
 {
@@ -24,6 +32,9 @@ int	main(int argc, char *argv[])
 	spawn_player(game);
 	mlx_loop_hook(game->mlx, &key_binds, game);
 	mlx_loop(game->mlx);
+
+	// check_for_leaks();
+	
 	mlx_terminate(game->mlx);
 	free_up(game);
 	return (0);
